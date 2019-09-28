dde7a57b31622304c51cf0776c38be87b1368839
Prachi Gaikwad <prachi321g@gmail.com>
2019-09-28 15:09:29 +0530

    Added Book.java

--- Book.java
+++ Book.java
@@ -3,4 +3,24 @@ class Book
 	private int bookId;
 	privat String name;
 
+	public void setBookId()
+	{
+		this.id=bookId;
+	}
+	
+	public int getBookId(int bookId)
+	{
+		return bookId;
+	}
+	
+	public void setName()
+	{
+		this.name=name;
+	}
+	
+	public String getName(String name)
+	{
+		return name;
+	}
+	
 }
