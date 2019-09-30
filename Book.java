91104673d30b13b6622e11973253c1c83e1cd533
Prachi Gaikwad <prachi321g@gmail.com>
2019-09-30 12:27:05 +0530

    Added author var in Book.java

--- Book.java
+++ Book.java
@@ -2,26 +2,37 @@ class Book
 {
 	private int bookId;
 	privat String name;
+	private String author;
 
-	public void setBookId()
+	public void setBookId(int bookId)
 	{
 		this.id=bookId
 		System.out.println("Book id is set");
 	}
 	
-	public int getBookId(int bookId)
+	public int getBookId()
 	{
 		return bookId;
 	}
 	
-	public void setName()
+	public void setName(String name)
 	{
 		this.name=name;
 	}
 	
-	public String getName(String name)
+	public String getName()
 	{
 		return name;
+	}
+	
+	public void setAuthor(String author)
+	{
+		this.author=author;
+	}
+	
+	public String getAuthor()
+	{
+		return author;
 	}
 	
 }
